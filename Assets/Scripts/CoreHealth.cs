d1a0876ad9e26b943854215fe6811ab71d2fbf4b
Egor <s2500798@edu.bc.fi>
2026-09-05 20:39:47 +0300

    added hpbar

--- Assets/Scripts/CoreHealth.cs
+++ Assets/Scripts/CoreHealth.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoreHealth : MonoBehaviour
 {
     [Header("Health Settings")]
     public float maxHealth = 100f;
     public float currentHealth;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private GameObject gamePanel;
+    [SerializeField] private GameObject deathScreenPanel;
+    [SerializeField] private GameObject mainMenuPanel;
 
     void Start()
     {
         currentHealth = maxHealth;
+        if (deathScreenPanel != null) deathScreenPanel.SetActive(false);
+        UpdateHealthUI();
+        ResetHealth();
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
     }
 
     public void TakeDamage(float amount)
@@ -21,16 +35,28 @@ public class CoreHealth : MonoBehaviour
         {
             GameOver();
         }
+        healthSlider.value = currentHealth / maxHealth;
     }
 
     public void Heal(float amount)
     {
         currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        healthSlider.value = currentHealth / maxHealth;
         Debug.Log($"Core Healed! Health: {currentHealth}/{maxHealth}");
+    }
+    private void UpdateHealthUI()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth / maxHealth;
+        }
     }
 
     private void GameOver()
     {
         Debug.Log("Core Destroyed! Game Over!");
+        if (gamePanel != null) gamePanel.SetActive(false);
+        if (deathScreenPanel != null) deathScreenPanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
